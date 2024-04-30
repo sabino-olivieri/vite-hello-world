a8a10c520a88d063e3664c6903f857fa3cf5958e
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-30 17:50:49 +0200

    feat: component AppTitle added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <script>
-    export default {
+import AppTitle from './components/AppTitle.vue'
 
-      data() {
-        return {
+export default {
 
-        }
-      },
+  components: {
+    AppTitle,
+  },
+
+  data() {
+    return {
+
     }
+  },
+}
 </script>
 
 <template>
 
+  <div class="container">
+
+    <AppTitle />
+    
+  </div>
+
 </template>
 
 <style>
-
+.container {
+  width: 70%;
+  margin: 20px auto;
+  background-color: bisque;
+  text-align: center;
+  border-radius: 10px;
+  padding: 20px;
+  font-family: 'Courier New', Courier, monospace;
+}
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<style>
+
+</style>
